5bda247fbaab0a17d29bc813a3fb18f38aa23fdc
funstuie-bit <264561520+funstuie-bit@users.noreply.github.com>
2026-09-15 11:49:30 -0700

    Pin Homebrew to 1.0.4

--- Formula/jukeboxcli.rb
+++ Formula/jukeboxcli.rb
@@ -2,9 +2,9 @@ class Jukeboxcli < Formula
   desc "Mac-first terminal music player with local and online queues"
   homepage "https://github.com/funstuie-bit/JukeboxCli"
   url "https://github.com/funstuie-bit/JukeboxCli.git",
-      tag: "v1.0.3",
-      revision: "1d8d9818a9f9e68fe9cf7658e0d8bc21276fa7ff"
-  version "1.0.3"
+      tag: "v1.0.4",
+      revision: "40547987ae41934335a5fcef4516cd62ca609222"
+  version "1.0.4"
   license "MIT"
 
   depends_on :macos
